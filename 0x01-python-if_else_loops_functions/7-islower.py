f3a621d3bca4cdaa556eb6e21b02411acdedddef
Incitatous <dcianberlin@gmail.com>
2017-01-03 14:51:07 -0800

    without builtins

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    if c.islower():
+    #if c.islower():
+    if ord(c) in range(ord('a'), ord('z') + 1):
+    #if ord('a') >= c <= ord('z')
         return True
-    elif c.isupper():
-        return False
+    return False
