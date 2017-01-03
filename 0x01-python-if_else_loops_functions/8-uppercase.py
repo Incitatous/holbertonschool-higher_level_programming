b4337912e5df3ab28e5f5908c3be58b3153289cb
Incitatous <dcianberlin@gmail.com>
2017-01-03 15:14:07 -0800

    8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for item in str:
-    if ord(item) > 90 && :
-        item = ord(item)
-         
-print(str)
+def uppercase(str):
+    for item in str:
+        if ord('a') <= ord(item) <= ord('z'):
+            print(chr(ord(item) - 32), end="")
+        else:
+            print(item, end="")
+    print("{}".format(""))
