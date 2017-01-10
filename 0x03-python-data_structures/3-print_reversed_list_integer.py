3e5f148aace4c3c35c76c4011508fa60a49b185c
Incitatous <dcianberlin@gmail.com>
2017-01-10 12:31:25 -0800

    str fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(list=[]):
     for i in list[::-1]:
-        print("{:d}".format(i))
+        # print("{:d}".format(i))
+        print(str.format(str(i)))
