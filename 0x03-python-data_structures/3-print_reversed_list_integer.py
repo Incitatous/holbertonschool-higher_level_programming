086c5718ec323d852bd0c38e5559be2304571138
Incitatous <dcianberlin@gmail.com>
2017-01-10 14:56:49 -0800

    second commit

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(list=[]):
     for i in list[::-1]:
-        # print("{:d}".format(i))
-        print(str.format(str(i)))
+        print("{:d}".format(i))
+        # print(str.format(str(i)))
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def new_in_list(list, idx, element):
     # Colon necessary because of the pointer
     myList = list[:]
-    if idx in myList:
+    if idx >= 0 and idx in myList:
         myList[idx] = element
     return myList
