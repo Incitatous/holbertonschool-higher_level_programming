7e5515303f093c043ab337621553cfd2b79da90d
Incitatous <dcianberlin@gmail.com>
2017-01-12 19:08:21 -0800

    fix list comprehension

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    for idx, val in enumerate(new_list):
-        if val is search:
-            new_list.pop(search)
-            new_list.insert(idx, replace)
-    return new_list
+    return [replace if i==search else i for i in my_list]
