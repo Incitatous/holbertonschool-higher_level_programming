df87406ad996c6d1ac73bb0bcc3cfa55e9910a9a
Incitatous <dcianberlin@gmail.com>
2017-01-18 13:15:32 -0800

    update

---
+++ 0x06-python-test_driven_development/1-main.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+mul = __import__('1-mul.py').mul
+print(mul(None, None))
---
+++ 0x06-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    if isinstance(text, str) is False:
+        raise TypeError("text must be a string")
+    else:
+        text = text.replace(". ", ".\n\n")
+        text = text.replace("? ", "?\n\n")
+        text = text.replace(": ", ":\n\n")
+        print("{:s}".format(text))
+    print("")
