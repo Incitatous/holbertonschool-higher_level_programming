6dc3e93aedaaa344ab3cb60ce5992aec90cc4000
Incitatous <dcianberlin@gmail.com>
2017-01-18 18:15:00 -0800

    rollback

--- 0x06-python-test_driven_development/0-add_integer.py
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_integer(a, b):
-    if isinstance(a, int) is False and isinstance(a, float) is False:
+    if isinstance(a, (int, float)) is False:
         raise TypeError("a must be an integer")
-    if isinstance(b, int) is False and isinstance(b, float) is False:
+    if isinstance(b, (int, float) is False:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
