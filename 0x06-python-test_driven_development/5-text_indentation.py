cf31bd54e57c678882de61b8714b0eefeece6dba
Incitatous <dcianberlin@gmail.com>
2017-01-18 20:06:17 -0800

    without else

--- 0x06-python-test_driven_development/5-text_indentation.py
+++ 0x06-python-test_driven_development/5-text_indentation.py
@@ -2,10 +2,9 @@
 def text_indentation(text):
     if isinstance(text, str) is False:
         raise TypeError("text must be a string")
-    else:
-        text = text.replace(". ", ".\n\n")
-        text = text.replace("? ", "?\n\n")
-        text = text.replace(": ", ":\n\n")
-        text = text.strip()
-        print("{:s}".format(text))
+    text = text.replace(". ", ".\n\n")
+    text = text.replace("? ", "?\n\n")
+    text = text.replace(": ", ":\n\n")
+    text = text.strip()
+    print("{:s}".format(text))
     print("")
