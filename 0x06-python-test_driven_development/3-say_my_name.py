193848676096e1774920512f9a7bb67d207aa1d1
Incitatous <dcianberlin@gmail.com>
2017-01-18 17:53:41 -0800

    updates and trimming

--- 0x06-python-test_driven_development/3-say_my_name.py
+++ 0x06-python-test_driven_development/3-say_my_name.py
@@ -4,11 +4,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if isinstance(last_name, str) is False:
         raise TypeError("last_name must be a string")
-    try:
-        print("My name is {:s} ".format(first_name), end="")
-    except Exception as err:
-        print(err)
-    try:
-        print("{:s}".format(last_name))
-    except Exception as err:
-        print(err)
+    print("My name is {:s} {:s}".format(first_name, last_name))
