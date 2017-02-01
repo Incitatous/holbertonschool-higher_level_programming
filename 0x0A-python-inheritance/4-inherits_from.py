2cd38e4fe02e6c4889428a5c36052583587f7834
Incitatous <dcianberlin@gmail.com>
2017-02-01 11:26:33 -0800

    fix3

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def inherits_from(obj, a_class):
-    if issublclass(obj,  a_class) or isinstance(obj, a_class):
+    if isinstance(obj,  a_class) and type(obj) is not a_class:
         return True
     return False
