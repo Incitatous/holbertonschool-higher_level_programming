e16a44573209e23d5c507c1a441fc4dec8b8b931
Incitatous <dcianberlin@gmail.com>
2017-02-07 15:18:18 -0800

    fix or statement

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -4,7 +4,7 @@ def read_lines(filename="", nb_lines=0):
     with open(filename) as fhandle:
         for line in fhandle:
             nb_lines += 1
-            if i <= nb_lines and nb_lines > 0:
+            if i <= nb_lines or nb_lines > 0:
                 print(line, end="")
             elif nb_lines <= 0:
                 print(line, end="")
