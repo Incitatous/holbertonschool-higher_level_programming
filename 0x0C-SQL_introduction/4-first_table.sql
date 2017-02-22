011e830a60544e4ce0f40ff7d4e24601d0780689
Incitatous <dcianberlin@gmail.com>
2017-02-22 21:39:47 +0000

    first commit

---
+++ 0x0C-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Creates a table called first_table in the database hbtn_0c_0
+USE hbtn_0c_0 IF EXISTS;
+CREATE TABLE IF NOT EXISTS first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));
