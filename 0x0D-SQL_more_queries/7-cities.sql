abd1aa6c188b98ec1fea02b2576234efa98dc948
Incitatous <dcianberlin@gmail.com>
2017-02-28 15:30:19 -0800

    referencing to table not database

--- 0x0D-SQL_more_queries/7-cities.sql
+++ 0x0D-SQL_more_queries/7-cities.sql
@@ -3,7 +3,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	state_id INT,
+	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa (id)
+	FOREIGN KEY (state_id)
+		REFERENCES hbtn_0d_usa.states (id)
 );
