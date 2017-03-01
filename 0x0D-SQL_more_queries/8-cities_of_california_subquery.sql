039c8f4f70b4bbdfb47288473e01904588b16cf6
Incitatous <dcianberlin@gmail.com>
2017-02-28 16:05:41 -0800

    removing path to table

--- 0x0D-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0D-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- Lists all the cities of California in hbtn_0d_usa
-SELECT id, name FROM hbtn_0d_usa.cities
+SELECT id, name FROM cities
 		WHERE state_id IN (
 			SELECT states.id
 			FROM states
