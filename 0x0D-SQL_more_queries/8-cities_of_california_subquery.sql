5f1047fc123bb40a720c80d834502f0c8cb3daa7
Incitatous <dcianberlin@gmail.com>
2017-02-28 16:04:02 -0800

    referring to table not database

--- 0x0D-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0D-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,8 @@
 -- Lists all the cities of California in hbtn_0d_usa
-SELECT
-		id, name
-		FROM hbtn_0d_usa
-		GROUP BY state('California')
+SELECT id, name FROM hbtn_0d_usa.cities
+		WHERE state_id IN (
+			SELECT states.id
+			FROM states
+			WHERE name = 'California'
+		)
 		ORDER BY id ASC;
