e1328cc03b5ef04d08221a8cdfb3d85070804860
Incitatous <dcianberlin@gmail.com>
2017-03-08 16:41:25 -0800

    task1 fix

--- 0x0E-python-object_relational_mapping/1-filter_states.py
+++ 0x0E-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
-db = MySQLdb.connect(user=sys.argv[1],
-                     host="localhost",
-                     passwd=sys.argv[2],
-                     db=sys.argv[3])
-myCursor = db.cursor()
-myCursor.execute("SELECT * FROM hbtn_0e_0_usa.states ORDER BY id  ASC")
-for i in myCursor.fetchall():
-    if i[1][0] == 'N':
-        print(i)
-db.close()
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(user=sys.argv[1],
+                         host="localhost",
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    myCursor = db.cursor()
+    myCursor.execute("SELECT * FROM states ORDER BY id  ASC")
+    for i in myCursor.fetchall():
+        if i[1][0] == 'N':
+            print(i)
+    db.close()
