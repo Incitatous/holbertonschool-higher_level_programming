a1631befd7df38974be203d0441fc2891b43840a
Incitatous <dcianberlin@gmail.com>
2017-03-08 16:31:04 -0800

    fix task0

--- 0x0E-python-object_relational_mapping/0-select_states.py
+++ 0x0E-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
-db = MySQLdb.connect(user=sys.argv[1],
-                     host="localhost",
-                     passwd=sys.argv[2],
-                     db=sys.argv[3])
-myCursor = db.cursor()
-myCursor.execute("SELECT * FROM hbtn_0e_0_usa.states ORDER BY id ASC")
-for i in myCursor.fetchall():
-    print(i)
-db.close()
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(user=sys.argv[1],
+                         host="localhost",
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    myCursor = db.cursor()
+    myCursor.execute("SELECT * FROM hbtn_0e_0_usa.states ORDER BY id ASC")
+    for i in myCursor.fetchall():
+        print(i)
+    db.close()
