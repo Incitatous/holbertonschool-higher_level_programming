200502b8136ecd62df2e8e80476694b4190941ce
Incitatous <dcianberlin@gmail.com>
2017-03-08 16:48:16 -0800

    task4 fix

--- 0x0E-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0E-python-object_relational_mapping/4-cities_by_state.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
-db = MySQLdb.connect(user=sys.argv[1],
-                     host="localhost",
-                     passwd=sys.argv[2],
-                     db=sys.argv[3])
-myCursor = db.cursor()
-myCursor.execute("SELECT cities.id, cities.name, states.name\
-                FROM cities JOIN states ON\
-                states.id = cities.state_id ORDER BY cities.id ASC")
-for i in myCursor.fetchall():
-    print(i)
-db.close()
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(user=sys.argv[1],
+                         host="localhost",
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    myCursor = db.cursor()
+    myCursor.execute("SELECT cities.id, cities.name, states.name\
+                    FROM cities JOIN states ON\
+                    states.id = cities.state_id ORDER BY cities.id ASC")
+    for i in myCursor.fetchall():
+        print(i)
+    db.close()
