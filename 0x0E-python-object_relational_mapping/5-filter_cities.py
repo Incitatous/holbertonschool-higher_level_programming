e30421b8b69db273e76d209403264b595b61af91
Incitatous <dcianberlin@gmail.com>
2017-03-08 18:25:25 -0800

    task5

--- 0x0E-python-object_relational_mapping/5-filter_cities.py
+++ 0x0E-python-object_relational_mapping/5-filter_cities.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
-db=MySQLdb.connect(user=sys.argv[1],
-                    host="localhost",
-                    passwd=sys.argv[2],
-                    db=sys.argv[3])
-myCursor = db.cursor()
-myCursor.execute("SELECT cities.name FROM hbtn_0e_0_usa.states JOIN states states.name = %s ORDER BY id ASC", (sys.argv[4],))
-for i in myCursor.fetchall():
-    print(i)
-db.close()
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(user=sys.argv[1],
+                         host="localhost",
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    myCursor = db.cursor()
+    myCursor.execute("SELECT * FROM cities\
+                    WHERE state_id IN\
+                    (SELECT id FROM states WHERE name = %s)\
+                    ORDER BY id ASC", (sys.argv[4],))
+    myResults = []
+    for i in myCursor.fetchall():
+        if i[2] not in myResults:
+            myResults.append(i[2])
+    print(', '.join(myResults))
+    db.close()
