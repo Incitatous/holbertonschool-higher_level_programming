3efa6cc1f3300bbff6fa105d4ad6b1c9ed2cb5e7
Incitatous <dcianberlin@gmail.com>
2017-03-08 22:59:39 -0800

    fixing indentation

--- 0x0E-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0E-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-for instance in session.query(State).order_by(State.id):
-    print("{}: {}".format(instance.id, instance.name))
+    for instance in session.query(State).order_by(State.id):
+        print("{}: {}".format(instance.id, instance.name))
