53fbde3038bd82eecde557f311dc9f048cb5ac7e
Incitatous <dcianberlin@gmail.com>
2017-04-10 22:26:08 -0700

    flag fixed

--- 0x0F-python-network_0/36-body_size.sh
+++ 0x0F-python-network_0/36-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays the size of the body of the response
-curl -Ils '$1' | grep Content-Length | awk -F  "Content-Length: " '{print $2}'
+curl -Is '$1' | grep Content-Length | awk -F  "Content-Length: " '{print $2}'
