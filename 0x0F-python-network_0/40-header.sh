c9d70bee9c05a8db69ced24bf8f90d97f2bf08b9
Incitatous <dcianberlin@gmail.com>
2017-04-12 14:52:19 -0700

    fix

--- 0x0F-python-network_0/40-header.sh
+++ 0x0F-python-network_0/40-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to the URL, and displays the body of the response
-curl -H "X-HolbertonSchool-User-Id: 98" 0.0.0.0:5000/route_5 ; echo ""
+curl -H "X-HolbertonSchool-User-Id: 98" "$1"
