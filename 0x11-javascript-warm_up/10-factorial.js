461be9e7a06abba03143d2f9bf850f3ddb074c1a
Incitatous <dcianberlin@gmail.com>
2017-04-27 18:56:18 -0700

    fix

--- 0x11-javascript-warm_up/10-factorial.js
+++ 0x11-javascript-warm_up/10-factorial.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
-if (!process.argv[2] || process.argv[2] < 2) {
-  console.log(1);
-} else {
-  let res = 1;
-  for (let num = process.argv[2]; num > 1; num--) {
-    res = res * num;
+function Fact (number) {
+  if (!process.argv[2] || process.argv[2] < 2) {
+    console.log(1);
+  } else {
+    let res = 1;
+    for (let num = process.argv[2]; num > 1; num--) {
+      res = res * num;
+    }
+    console.log(res);
   }
-  console.log(res);
 }
+Fact(parseInt(process.argv[2]));
