fd963f571f24dde674f249c75f1b3b321f4000b8
Incitatous <dcianberlin@gmail.com>
2017-04-27 18:58:47 -0700

    accounting for NaN

--- 0x11-javascript-warm_up/10-factorial.js
+++ 0x11-javascript-warm_up/10-factorial.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 function Fact (number) {
-  if (!process.argv[2] || process.argv[2] < 2) {
+  if (!process.argv[2] || process.argv[2] < 2 || isNaN(process.argv[2])) {
     console.log(1);
   } else {
     let res = 1;
