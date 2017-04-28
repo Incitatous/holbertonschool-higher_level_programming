9154959432fd455e357179aff3d5e19d114fcd10
Incitatous <dcianberlin@gmail.com>
2017-04-27 18:49:19 -0700

    task10

--- 0x11-javascript-warm_up/10-factorial.js
+++ 0x11-javascript-warm_up/10-factorial.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-if (!process.argv[2]) {
-    console.log(1);
-}
-else {
-    let res = 0;
-    let num =  process.argv[2];
-    for (i = process.argv[2]; i > 0; i--) {
-        res += num * (num - 1);
-        num = num - 1;
-    }
-    console.log(num)
+if (!process.argv[2] || process.argv[2] < 2) {
+  console.log(1);
+} else {
+  let res = 1;
+  for (let num = process.argv[2]; num > 1; num--) {
+    res = res * num;
+  }
+  console.log(res);
 }
