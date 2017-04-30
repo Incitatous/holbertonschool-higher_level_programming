7d52351f73a0b13547af6cd2a1603e48a830f6d6
Incitatous <dcianberlin@gmail.com>
2017-04-30 15:15:13 -0700

    format

--- 0x12-javascript-web_scraping/2-rectangle.js
+++ 0x12-javascript-web_scraping/2-rectangle.js
@@ -2,8 +2,8 @@
 /* this keyword refers to the current
 object the code is being written inside */
 exports.Rectangle = function Rectangle (w, h) {
-    if (w > 0 && h > 0) {
+  if (w > 0 && h > 0) {
     this.width = w;
     this.height = h;
-    }
+  }
 };
