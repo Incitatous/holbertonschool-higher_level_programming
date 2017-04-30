fa77a6e208f1e143cfa7b14821d78a8953871376
Incitatous <dcianberlin@gmail.com>
2017-04-30 15:16:48 -0700

    format

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -3,5 +3,5 @@ const Rectangle = require('./4-rectangle').Rectangle;
 /* "call" calls a function with a
 given this value and arguments provided individually */
 exports.Square = function Square (size) {
-    Rectangle.call(this, size, size);
+  Rectangle.call(this, size, size);
 };
